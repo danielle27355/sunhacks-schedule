ebd6f9db3d33f503e8695aed1cc384acfb6fc966
danielle27355 <danielle27355@gmail.com>
2019-09-21 11:53:00 -0700

    first commit

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Nav.css";
+
+class Nav extends Component {
+    render() {
+
+        return(
+            <div className="Nav">
+                Meow
+            </div>
+        );
+       
+    }
+}
+
+export default Nav;
